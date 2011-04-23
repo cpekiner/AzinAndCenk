9217810f9e0ac90f235d1ed537db3f2fb129ab9f
Cenk Pekiner <muratbatal@gmail.com>
2011-04-22 23:32:20 -0700

    first changes

--- ac.web/Master/ac.Master.designer.cs
+++ ac.web/Master/ac.Master.designer.cs
@@ -22,6 +22,15 @@ namespace ac.web.Master {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder cpHead;
         
         /// <summary>
+        /// basestyle control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlLink basestyle;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
