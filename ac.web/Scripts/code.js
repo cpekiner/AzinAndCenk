c31d321945376c5c4dafbe3d48cb994dcb1aad63
Cenk Pekiner <cenk@.nvidia.com>
2011-04-22 23:10:44 -0700

    committing from my desktop

---
+++ ac.web/Scripts/code.js
@@ -0,0 +1,18 @@
+﻿
+$(document).ready
+    (
+        function () {
+            $('ul.nav a').click(function () {
+                var curChildIndex = $(this).parent().prevAll().length + 1;                                
+                $(this).parent().parent().children('.current').removeClass('current');
+                $(this).parent().addClass('current');
+                $(this).parent().parent().next('.tabcontainer').children('.current').slideUp('fast', function () {
+                    $(this).removeClass('current');
+                    $(this).parent().children('div:nth-child(' + curChildIndex + ')').slideDown('normal', function () {
+                        $(this).addClass('current');
+                    });
+                });
+                return false;
+            });
+        }
+    );
